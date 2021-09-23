848fdc1c52bf1e00762188515293d499390c444c
aguilarluisdi@hotmail.com <aguilarluisdi@hotmail.com>
2021-09-22 19:56:59 -0600

    Update CompositeProduct.java

--- CompositePattern/src/compositepattern/CompositeProduct.java
+++ CompositePattern/src/compositepattern/CompositeProduct.java
@@ -5,10 +5,38 @@
  */
 package compositepattern;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Natalia
  */
-public class CompositeProduct {
+public class CompositeProduct extends AbstractProduct {
+    public ArrayList<AbstractProduct> children;
     
+    public CompositeProduct(String pName, double pPrice){
+        this.name = pName;
+        this.price = pPrice;
+    }
+
+    public ArrayList<AbstractProduct> getChildren() {
+        return children;
+    }
+
+    public void setChildren(ArrayList<AbstractProduct> pChildren) {
+        this.children = pChildren;
+    }
+    
+    public void addProduct(AbstractProduct pProduct){
+        this.children.add(pProduct);
+    }
+    
+    @Override
+    public double getPrice(){
+        double total = this.price;
+        for(AbstractProduct product : this.children){
+            total += product.getPrice();
+        }
+        return total;
+    }
 }
