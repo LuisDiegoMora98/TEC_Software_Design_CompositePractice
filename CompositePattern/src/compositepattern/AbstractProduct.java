1c615ed917011026189a0328fc83a3c22065c0f2
Hawk-chan <30329240+natalia0911@users.noreply.github.com>
2021-09-22 19:43:28 -0600

    Update AbstractProduct.java

--- CompositePattern/src/compositepattern/AbstractProduct.java
+++ CompositePattern/src/compositepattern/AbstractProduct.java
@@ -9,6 +9,33 @@ package compositepattern;
  *
  * @author Natalia
  */
-public class AbstractProduct {
+public abstract class AbstractProduct {
+    protected String name;
+    protected double price;
+
+    public AbstractProduct(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public AbstractProduct() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
     
 }
